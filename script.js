2b2d0bfcceb83c8e3fe81b51a193b62ecd3f4780
John Bede <johnbede2000@gmail.com>
2020-09-09 16:01:10 +0100

    plans clearer

--- script.js
+++ script.js
@@ -3,23 +3,32 @@ for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', playRound);
 }
 
+let playerTotal = 0;
+let compTotal = 0;
+let roundText = document.querySelector('#round-text');
+
 function playRound(e) {
     let playerSelection = this.id;
     let compSelection = generateCompSelection;
+    let roundText = '';
 
-    if (playerSelection === 'Rock' && compSelection === 'Rock') { return 'A draw!'; }
-    else if (playerSelection === 'Rock' && compSelection === 'Paper') { compTotal++; return 'You lose! Paper beats rock'; }
-    else if (playerSelection === 'Rock' && compSelection === 'Scissors') { playerTotal++; return 'You win! Rock beats scissors'; }
-    else if (playerSelection === 'Paper' && compSelection === 'Rock') { playerTotal++; return 'You win! Paper beats rock'; }
-    else if (playerSelection === 'Paper' && compSelection === 'Paper') { return 'A draw!'; }
-    else if (playerSelection === 'Paper' && compSelection === 'Scissors') { compTotal++; return 'You lose! Scissors beats paper!'; }
-    else if (playerSelection === 'Scissors' && compSelection === 'Rock') { compTotal++; return 'You lose! Rock beats scissors!'; }
-    else if (playerSelection === 'Scissors' && compSelection === 'Paper') { playerTotal++; return 'You win! Scissors beats paper!'; }
-    else if (playerSelection === 'Scissors' && compSelection === 'Scissors') { return 'A draw!'; }
+    if (playerSelection === 'Rock' && compSelection === 'Rock') { roundText = 'A draw!'; }
+    else if (playerSelection === 'Rock' && compSelection === 'Paper') { compTotal++; roundText = 'You lose! Paper beats rock'; }
+    else if (playerSelection === 'Rock' && compSelection === 'Scissors') { playerTotal++; roundText = 'You win! Rock beats scissors'; }
+    else if (playerSelection === 'Paper' && compSelection === 'Rock') { playerTotal++; roundText = 'You win! Paper beats rock'; }
+    else if (playerSelection === 'Paper' && compSelection === 'Paper') { roundText = 'A draw!'; }
+    else if (playerSelection === 'Paper' && compSelection === 'Scissors') { compTotal++; roundText = 'You lose! Scissors beats paper!'; }
+    else if (playerSelection === 'Scissors' && compSelection === 'Rock') { compTotal++; roundText = 'You lose! Rock beats scissors!'; }
+    else if (playerSelection === 'Scissors' && compSelection === 'Paper') { playerTotal++; roundText = 'You win! Scissors beats paper!'; }
+    else if (playerSelection === 'Scissors' && compSelection === 'Scissors') { roundText = 'A draw!'; }
+
+    // display the updated score ('playerTotal / compTotal')
+    // display roundText
+    // display human selection big
+    // display computer selection big
+    // stop after someone gets 5 and display winnerText after (or instead of) roundText
+    // reset button triggers reset function
 }
-
-let playerTotal = 0;
-let compTotal = 0;
 
 function generateCompSelection() {
     let randomNumber = Math.floor((3 - 1 + 1) * Math.random()) + 1;
@@ -27,38 +36,3 @@ function generateCompSelection() {
     else if (randomNumber === 2) { return 'Paper'; }
     else { return 'Scissors'; }
 }
-
-// function game() {
-//     let playerSelection = prompt('Round 1/5! Enter your selection (rock, paper, or scissors)');
-//     let compSelection = generateCompSelection();
-//     console.log(playRound(playerSelection, compSelection) + '. Player ' + playerTotal + ', Computer ' + compTotal + '.');
-
-//     playerSelection = prompt('Round 2/5! Enter your selection (rock, paper, or scissors)');
-//     compSelection = generateCompSelection();
-//     console.log(playRound(playerSelection, compSelection) + '. Player ' + playerTotal + ', Computer ' + compTotal + '.');
-
-//     playerSelection = prompt('Round 3/5! Enter your selection (rock, paper, or scissors)');
-//     compSelection = generateCompSelection();
-//     console.log(playRound(playerSelection, compSelection) + '. Player ' + playerTotal + ', Computer ' + compTotal + '.');
-
-//     playerSelection = prompt('Round 4/5! Enter your selection (rock, paper, or scissors)');
-//     compSelection = generateCompSelection();
-//     console.log(playRound(playerSelection, compSelection) + '. Player ' + playerTotal + ', Computer ' + compTotal + '.');
-
-//     playerSelection = prompt('Final round! Enter your selection (rock, paper, or scissors)');
-//     compSelection = generateCompSelection();
-//     console.log(playRound(playerSelection, compSelection) + '. Final score: Player ' + playerTotal + ', Computer ' + compTotal + '.')
-
-//     if (playerTotal > compTotal) {
-//     console.log('You win the game!');
-//     }
-//     else if (playerTotal < compTotal) {
-//     console.log('You lost the game :(');
-//     }
-//     else {
-//     console.log('The game was a draw.');
-//     }
-
-//     playerTotal = 0;
-//     compTotal = 0;
-// }
