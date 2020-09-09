f30261608a9ee9ed0612dc49423a29d78b32c3db
John Bede <johnbede2000@gmail.com>
2020-09-09 14:10:55 +0100

    playRound function

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const buttons = document.querySelectorAll('button');
 for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener(click, playRound);
+    buttons[i].addEventListener('click', playRound);
 }
 
-function playRound(playerSelection, compSelection) {
-    let playerSelection = e.target;
-    if (playerSelection.toUpperCase() === 'ROCK' && compSelection === 'Rock') { return 'A draw! Rock draws with rock'; }
-    else if (playerSelection.toUpperCase() === 'ROCK' && compSelection === 'Paper') { compTotal++; return 'You lose! Paper beats rock'; }
-    else if (playerSelection.toUpperCase() === 'ROCK' && compSelection === 'Scissors') { playerTotal++; return 'You win! Rock beats scissors'; }
-    else if (playerSelection.toUpperCase() === 'PAPER' && compSelection === 'Rock') { playerTotal++; return 'You win! Paper beats rock'; }
-    else if (playerSelection.toUpperCase() === 'PAPER' && compSelection === 'Paper') { return 'A draw! Paper draws with paper!'; }
-    else if (playerSelection.toUpperCase() === 'PAPER' && compSelection === 'Scissors') { compTotal++; return 'You lose! Scissors beats paper!'; }
-    else if (playerSelection.toUpperCase() === 'SCISSORS' && compSelection === 'Rock') { compTotal++; return 'You lose! Rock beats scissors!'; }
-    else if (playerSelection.toUpperCase() === 'SCISSORS' && compSelection === 'Paper') { playerTotal++; return 'You win! Scissors beats paper!'; }
-    else if (playerSelection.toUpperCase() === 'SCISSORS' && compSelection === 'Scissors') { return 'A draw! Scissors draws with scissors!'; }
+function playRound(e) {
+    
+    let playerSelection = this.id;
+    let compSelection = generateCompSelection;
+
+    if (playerSelection === 'Rock' && compSelection === 'Rock') { return 'A draw!'; }
+    else if (playerSelection === 'Rock' && compSelection === 'Paper') { compTotal++; return 'You lose! Paper beats rock'; }
+    else if (playerSelection === 'Rock' && compSelection === 'Scissors') { playerTotal++; return 'You win! Rock beats scissors'; }
+    else if (playerSelection === 'Paper' && compSelection === 'Rock') { playerTotal++; return 'You win! Paper beats rock'; }
+    else if (playerSelection === 'Paper' && compSelection === 'Paper') { return 'A draw!'; }
+    else if (playerSelection === 'Paper' && compSelection === 'Scissors') { compTotal++; return 'You lose! Scissors beats paper!'; }
+    else if (playerSelection === 'Scissors' && compSelection === 'Rock') { compTotal++; return 'You lose! Rock beats scissors!'; }
+    else if (playerSelection === 'Scissors' && compSelection === 'Paper') { playerTotal++; return 'You win! Scissors beats paper!'; }
+    else if (playerSelection === 'Scissors' && compSelection === 'Scissors') { return 'A draw!'; }
 }
 
 let playerTotal = 0;
@@ -26,37 +29,37 @@ function generateCompSelection() {
     else { return 'Scissors'; }
 }
 
-function game() {
-    let playerSelection = prompt('Round 1/5! Enter your selection (rock, paper, or scissors)');
-    let compSelection = generateCompSelection();
-    console.log(playRound(playerSelection, compSelection) + '. Player ' + playerTotal + ', Computer ' + compTotal + '.');
+// function game() {
+//     let playerSelection = prompt('Round 1/5! Enter your selection (rock, paper, or scissors)');
+//     let compSelection = generateCompSelection();
+//     console.log(playRound(playerSelection, compSelection) + '. Player ' + playerTotal + ', Computer ' + compTotal + '.');
 
-    playerSelection = prompt('Round 2/5! Enter your selection (rock, paper, or scissors)');
-    compSelection = generateCompSelection();
-    console.log(playRound(playerSelection, compSelection) + '. Player ' + playerTotal + ', Computer ' + compTotal + '.');
+//     playerSelection = prompt('Round 2/5! Enter your selection (rock, paper, or scissors)');
+//     compSelection = generateCompSelection();
+//     console.log(playRound(playerSelection, compSelection) + '. Player ' + playerTotal + ', Computer ' + compTotal + '.');
 
-    playerSelection = prompt('Round 3/5! Enter your selection (rock, paper, or scissors)');
-    compSelection = generateCompSelection();
-    console.log(playRound(playerSelection, compSelection) + '. Player ' + playerTotal + ', Computer ' + compTotal + '.');
+//     playerSelection = prompt('Round 3/5! Enter your selection (rock, paper, or scissors)');
+//     compSelection = generateCompSelection();
+//     console.log(playRound(playerSelection, compSelection) + '. Player ' + playerTotal + ', Computer ' + compTotal + '.');
 
-    playerSelection = prompt('Round 4/5! Enter your selection (rock, paper, or scissors)');
-    compSelection = generateCompSelection();
-    console.log(playRound(playerSelection, compSelection) + '. Player ' + playerTotal + ', Computer ' + compTotal + '.');
+//     playerSelection = prompt('Round 4/5! Enter your selection (rock, paper, or scissors)');
+//     compSelection = generateCompSelection();
+//     console.log(playRound(playerSelection, compSelection) + '. Player ' + playerTotal + ', Computer ' + compTotal + '.');
 
-    playerSelection = prompt('Final round! Enter your selection (rock, paper, or scissors)');
-    compSelection = generateCompSelection();
-    console.log(playRound(playerSelection, compSelection) + '. Final score: Player ' + playerTotal + ', Computer ' + compTotal + '.')
+//     playerSelection = prompt('Final round! Enter your selection (rock, paper, or scissors)');
+//     compSelection = generateCompSelection();
+//     console.log(playRound(playerSelection, compSelection) + '. Final score: Player ' + playerTotal + ', Computer ' + compTotal + '.')
 
-    if (playerTotal > compTotal) {
-    console.log('You win the game!');
-    }
-    else if (playerTotal < compTotal) {
-    console.log('You lost the game :(');
-    }
-    else {
-    console.log('The game was a draw.');
-    }
+//     if (playerTotal > compTotal) {
+//     console.log('You win the game!');
+//     }
+//     else if (playerTotal < compTotal) {
+//     console.log('You lost the game :(');
+//     }
+//     else {
+//     console.log('The game was a draw.');
+//     }
 
-    playerTotal = 0;
-    compTotal = 0;
-}
+//     playerTotal = 0;
+//     compTotal = 0;
+// }
