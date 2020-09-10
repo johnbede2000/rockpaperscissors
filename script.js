ac5f7fd626ad3896acea5c2a8daa797a4f028234
John Bede <johnbede2000@gmail.com>
2020-09-10 20:52:52 +0100

    game stops at 5 points

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('.button');
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', playRound);
 }
@@ -13,75 +13,98 @@ function generateCompSelection() {
     else { return 'Scissors'; }
 }
 
+let roundText = '';
+
 function playRound(e) {
     let playerSelection = this.id;
     let compSelection = generateCompSelection();
-    let roundText = '';
     document.querySelector('#human-current').className = "";
     document.querySelector('#comp-current').className = "";
 
     if (playerSelection === 'Rock' && compSelection === 'Rock') {
-        roundText = 'A draw!';
+        roundText = 'This round is a draw!';
         document.querySelector('#human-current').classList.add('rock-img');
-        
         document.querySelector('#comp-current').classList.add('rock-img');
     }
     else if (playerSelection === 'Rock' && compSelection === 'Paper') {
         compTotal++;
-        roundText = 'You lose! Paper beats rock';
+        roundText = 'You lose this round! Paper beats rock.';
         document.querySelector('#human-current').classList.add('rock-img');
         document.querySelector('#comp-current').classList.add('paper-img');
     }
     else if (playerSelection === 'Rock' && compSelection === 'Scissors') {
         playerTotal++;
-        roundText = 'You win! Rock beats scissors';
+        roundText = 'You win this round! Rock beats scissors.';
         document.querySelector('#human-current').classList.add('rock-img');
         document.querySelector('#comp-current').classList.add('scissors-img');
     }
     else if (playerSelection === 'Paper' && compSelection === 'Rock') {
         playerTotal++;
-        roundText = 'You win! Paper beats rock';
+        roundText = 'You win this round! Paper beats rock.';
         document.querySelector('#comp-current').classList.add('rock-img');
         document.querySelector('#human-current').classList.add('paper-img');
     }
     else if (playerSelection === 'Paper' && compSelection === 'Paper') {
-        roundText = 'A draw!';
+        roundText = 'This round is a draw!';
         document.querySelector('#human-current').classList.add('paper-img');
         document.querySelector('#comp-current').classList.add('paper-img');
     }
     else if (playerSelection === 'Paper' && compSelection === 'Scissors') {
         compTotal++;
-        roundText = 'You lose! Scissors beats paper!';
+        roundText = 'You lose this round! Scissors beats paper.';
         document.querySelector('#human-current').classList.add('paper-img');
         document.querySelector('#comp-current').classList.add('scissors-img');
     }
     else if (playerSelection === 'Scissors' && compSelection === 'Rock') {
         compTotal++;
-        roundText = 'You lose! Rock beats scissors!';
+        roundText = 'You lose this round! Rock beats scissors.';
         document.querySelector('#comp-current').classList.add('rock-img');
         document.querySelector('#human-current').classList.add('scissors-img');
     }
     else if (playerSelection === 'Scissors' && compSelection === 'Paper') {
         playerTotal++;
-        roundText = 'You win! Scissors beats paper!';
+        roundText = 'You win this round! Scissors beats paper.';
         document.querySelector('#human-current').classList.add('scissors-img');
         document.querySelector('#comp-current').classList.add('paper-img');
     }
     else if (playerSelection === 'Scissors' && compSelection === 'Scissors') {
-        roundText = 'A draw!';
+        roundText = 'This round is a draw!';
         document.querySelector('#human-current').classList.add('scissors-img');
         document.querySelector('#comp-current').classList.add('scissors-img');
     }
     else {
-        roundText = 'Oh... something went wrong!';
+        roundText = 'Oh... something went wrong.';
     }
-    
-    document.querySelector('#score').textContent = playerTotal + ' / ' + compTotal;
-    
-    document.querySelector('#round-text').textContent = roundText;
 
-    
-    // stop the game after someone gets 5 and display winnerText after (or instead of) roundText
-    // reset button triggers reset function
-    // if they try to play a round when winnerText is showing then reset the game first
+    document.querySelector('#score').textContent = playerTotal + ' / ' + compTotal;
+    checkForFive();
+    document.querySelector('#round-text').textContent = roundText;
 }
+
+function checkForFive() {
+    if (playerTotal === 5) {
+        roundText = roundText + ' Game over! You win this game!';
+        buttons.forEach(toggleHidden);
+        document.querySelector('#prompt').classList.toggle('hidden');
+    } else if (compTotal === 5) {
+        roundText = roundText + ' Game over! You lose this game! Try again';
+        buttons.forEach(toggleHidden);
+        document.querySelector('#prompt').classList.toggle('hidden');
+    }
+}
+
+function toggleHidden(currentValue) {
+    currentValue.classList.toggle('hidden');
+}
+
+function reset() {
+    playerTotal = 0;
+    compTotal = 0;
+    document.querySelector('#score').textContent = playerTotal + ' / ' + compTotal;
+    document.querySelector('#human-current').className = "";
+    document.querySelector('#comp-current').className = "";
+    document.querySelector('#round-text').textContent = "";
+    buttons.forEach(toggleHidden);
+    document.querySelector('#prompt').classList.toggle('hidden');
+}
+document.querySelector('#reset').addEventListener('click', reset);
